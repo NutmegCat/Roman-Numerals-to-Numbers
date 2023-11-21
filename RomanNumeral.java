dd750c47c6b167d16ed2160e8eee8bf8f76f4a35
NutmegCat <aaronprince.anu@student.tdsb.on.ca>
2023-11-21 10:47:29 -0500

    done main + romanToInteger

---
+++ RomanNumeral.java
@@ -0,0 +1,14 @@
+class RomanNumeral {
+    public static void main(String[] args) {
+        System.out.println("Enter a Roman numeral: ");
+        String romanNumeral = In.getString();
+        int integerValue = romanToInteger(romanNumeral);
+
+        System.out.println("Roman numeral: " + romanNumeral);
+        System.out.println("Number: " + integerValue);
+    }
+
+    public static int romanToInteger(String roman) {
+        
+    }
+}
