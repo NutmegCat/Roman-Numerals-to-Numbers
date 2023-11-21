6d12e3696b95f75b47d787b12114a6b74df82b95
NutmegCat <aaronprince.anu@student.tdsb.on.ca>
2023-11-21 10:54:25 -0500

    romanToInteger + symbolToValue

--- RomanNumeral.java
+++ RomanNumeral.java
@@ -9,6 +9,24 @@ class RomanNumeral {
     }
 
     public static int romanToInteger(String roman) {
+        int result = 0;
+        int prevValue = 0;
+
+        for (int i = roman.length() - 1; i >= 0; i--) {
+            char currentSymbol = roman.charAt(i);
+            int currentValue = symbolToValue(currentSymbol);
+
+            if (currentValue < prevValue) {
+                result -= currentValue;
+            } else {
+                result += currentValue;
+            }
+            prevValue = currentValue;
+        }
+        return result;
+    }
+
+    public static int symbolToValue(char symbol) {
         
     }
 }
